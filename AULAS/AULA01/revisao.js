19f5547c670d9267f70d06bb8244f70c2cdada6f
Joao <joaopaulomedeiros2021@gmail.com>
2026-02-19 11:06:20 -0300

    revisão aula 1

--- AULAS/AULA01/revisao.js
+++ AULAS/AULA01/revisao.js
@@ -3,7 +3,7 @@ let a = 'texto1'
 
 let b =[true, 100, "txt", null, undefined, NaN]
 
-let //escopo de bloco
+//let a //escopo de bloco, let não permite a variaveis iguai
 var c //escopo global
 const Pi = 3.14 //valor nao editavel
 
